3d0ee3693c3ec91e92e6d83477f39028b659bede
Cooper D'Andilly <151992386+CooperDActor-bytes@users.noreply.github.com>
2024-07-05 17:46:25 +1000

    Create trafficlight.js

---
+++ trafficlight.js
@@ -0,0 +1,20 @@
+const red = document.getElementById("red");
+const yellow = document.getElementById("yellow");
+const green = document.getElementById("green");
+
+function turnOnLight(light) {
+  light.style.opacity = 1;
+}
+
+function handleKeyPress(event) {
+  const numberPressed = event.key;
+  if (numberPressed === "1") {
+    turnOnLight(red);
+  } else if (numberPressed === "2") {
+    turnOnLight(yellow);
+  } else if (numberPressed === "3") {
+    turnOnLight(green);
+  }
+}
+
+document.addEventListener("keypress", handleKeyPress);
